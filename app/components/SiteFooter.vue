9f19070614aa7e0000f31c4aa93a7f6a70b79dc5
Lachsmc <146404835+Lechsmc@users.noreply.github.com>
2026-04-18 17:43:39 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -16,7 +16,11 @@
     </n-flex>
     <n-flex :size="4" class="text" align="center" vertical>
       <n-p depth="3">
-        <n-text depth="3" @click="jumpLink(linkData.github)">
+        <n-text depth="3" @click="jumpLink(metaData.project)">
+
+          SiteStatus
+
+        </n-text>
           SiteStatus
         </n-text>
         Version {{ version }}
@@ -32,7 +36,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(metaData.author)"> IMSYY </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -50,9 +54,14 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/",
+  home: "https://xilian.cyou",
+  email: "lechsmc697@163.com",
+};
+
+  const metaData = {
+  author: "https://imsyy.top",
+  project: "https://github.com/imsyy/site-status",
 };
 </script>
 
